65b5307c07c276911eaf4af691845347ce3d5162
abhayjadhav1234567 <anj4591217@gmail.com>
2025-08-19 00:13:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Countdown to next event
-const eventDate = new Date("2023-10-20");
-const today = new Date();
-const daysLeft = Math.ceil((eventDate - today) / (1000 * 60 * 60 * 24));
-document.getElementById("countdown").innerText = `${daysLeft} days left!`;
